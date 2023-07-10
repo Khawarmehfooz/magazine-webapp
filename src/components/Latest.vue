8a66d5281191ee835ac488fd2c3a531c1660310d
Khawarmehfooz <khawarmehfooz565@gmail.com>
2023-07-10 15:03:34 +0500

    Update Latest Component

--- src/components/Latest.vue
+++ src/components/Latest.vue
@@ -1,4 +1,40 @@
 <script>
+export default{
+    data(){
+        return{
+            latestArticles: [
+                {
+                    articleTitle: "The Definitive Helmut Newton Photobook is Reissued-in Miniature-for His 100th Birthday",
+                    articleExcerpt: "The Photographer, whose provocative work rose to prominence in the 1970s against a backdrop of the second-wave feminist movement, was one of the most impactful visual creators of his time.",
+                    articleAuthor: "Elen Jhosh",
+                    articleImage:"../../src/assets/images/Meghan-and-Harry.jpeg",
+                    articleDate: "Feb 12"
+                },
+                {
+                    articleTitle: "Made You Look: A True Story About Fake Art - When Working to Believe Is Just Not Enought",
+                    articleExcerpt: "The story of the most significant art fraud in history where ruined reputations and wounded egos mattered more than money.",
+                    articleAuthor: "Elen Jhosh",
+                    articleImage: "../../src/assets/images/Meghan-and-Harry.jpeg",
+                    articleDate: "Feb 12"
+                },
+                {
+                    articleTitle: "5 Things White People Keep Getting Totally Wrong About Racism",
+                    articleExcerpt: "They want to escape the harsh, unforgiving glare of culpability by pretending they've done nothing wrong, that if there are ongoing issues with racism in this country, it has nothing to do with how they raise their children. It's because black people refuse to move on.",
+                    articleAuthor: "Elen Jhosh",
+                    articleImage: "../../src/assets/images/Meghan-and-Harry.jpeg",
+                    articleDate: "Feb 12"
+                },
+                {
+                    articleTitle: "In London, the Most Famous \"Great Women Artist\" Finally Gets Her Due",
+                    articleExcerpt: "In July 2018, London's National Gallery Acquired a self-portrait by the Baroque artist Artemisia Gentileschi, featuring the painter in the guise of Sait Catherine of Alexandria. Almost immediately, it drew an unpresedented level of media attention.",
+                    articleAuthor: "Elen Jhosh",
+                    articleImage: "../../src/assets/images/Meghan-and-Harry.jpeg",
+                    articleDate: "Feb 12"
+                }
+            ]
+        }
+    }
+}
 </script>
 <template>
     <div class="latest__articles">
@@ -6,21 +42,28 @@
             <h2 class="latest__header__title">The Latest Articles</h2>
             <a href="#">View all</a>
         </div>
-        <article class="latest__article">
+        <article v-for="latestArticle in latestArticles" class="latest__article">
             <figure>
-                <img class="article__img" src="../assets/images/Meghan-and-Harry.jpeg" alt="">
+                <img class="article__img" :src="latestArticle.articleImage" alt="">
+                <!-- <img src="../../src/assets/images/Meghan-and-Harry.jpeg" alt=""> -->
             </figure>
             <div class="latest__article__content">
                 <div class="article__author">
-                    <p class="article__author__name">By Elen Jhosh</p>
+                    <p class="article__author__name">By {{ latestArticle.articleAuthor }}</p>
                 </div>
-                <h3 class="article__title">The Definitive Helmut Newton Photobook is Reissued-in Miniature-for His 100th Birthday</h3>
-                <p class="article__excerpt">The Photographer, whose provocative work rose to prominence in the 1970s against a backdrop of the second-wave feminist movement, was one of the most impactful visual creators of his time.</p>
+                <h3 class="article__title">{{ latestArticle.articleTitle }}</h3>
+                <p class="article__excerpt">{{ latestArticle.articleExcerpt }}</p>
             </div>
         </article>
     </div>
 </template>
 <style scoped>
+.latest__articles{
+    border-bottom: 1px solid grey;
+    margin-bottom: 2rem;
+    padding-bottom: 2rem;
+
+}
 .latest__header{
     border-top: 1px solid grey;
     border-bottom: 1px solid grey;
@@ -43,6 +86,7 @@
     display: grid;
     grid-template-columns: 1fr 2fr;
     gap: 2rem;
+    margin-bottom: 2rem;
 }
 .article__author{
     margin-bottom: 1.5rem;
@@ -60,6 +104,7 @@
 .article__author__name{
     padding-left: 40px;
     color: grey;
+    text-transform: uppercase;
 }
 .article__title{
     font-size: 1.8rem;
@@ -83,7 +128,7 @@
 }
 @media only screen and (max-width:800px){
     .article__img{
-        width: 280px;
+        width: 320px;
     }
     .article__title{
         font-size: 1.3rem;
