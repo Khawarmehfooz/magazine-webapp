b81a9fc43caf7176dfd4ce268e16b3101f5af16c
Khawarmehfooz <khawarmehfooz565@gmail.com>
2023-07-12 18:10:16 +0500

    Update Content of Latest Podcasts

--- src/components/LatestPodcasts.vue
+++ src/components/LatestPodcasts.vue
@@ -2,34 +2,27 @@
 export default {
     data() {
         return {
-            latestArticles: [
+            latestPodcasts: [
                 {
-                    articleTitle: "The Definitive Helmut Newton Photobook is Reissued-in Miniature-for His 100th Birthday",
-                    articleExcerpt: "The Photographer, whose provocative work rose to prominence in the 1970s against a backdrop of the second-wave feminist movement, was one of the most impactful visual creators of his time.",
-                    articleAuthor: "Elen Jhosh",
-                    articleImage: "/images/Meghan-and-Harry.jpeg",
-                    articleDate: "Feb 12"
+                    podcastTitle: "How are the boundaries between masculine and femine being erased in fashion?",
+                    podcastExcerpt: "Today, you will not surprise anyone with pale pink shades of clothing in the men's department. Men in dresses appear on the red carpets, and big brands like Chanel are launching lines of men's makeup.",
+                    podcastAuthor: "Elen Jhosh",
+                    podcastImage: "/images/Meghan-and-Harry.jpeg",
+                    podcastDate: "Feb 12"
                 },
                 {
-                    articleTitle: "Made You Look: A True Story About Fake Art - When Working to Believe Is Just Not Enought",
-                    articleExcerpt: "The story of the most significant art fraud in history where ruined reputations and wounded egos mattered more than money.",
-                    articleAuthor: "Elen Jhosh",
-                    articleImage: "/images/Meghan-and-Harry.jpeg",
-                    articleDate: "Feb 12"
+                    podcastTitle: "What is the essence of Gandhi's philosophy?",
+                    podcastExcerpt: "It so happes that a proper name becomes a household name. This happened with the name of Mahatma Gandhi. Neither before nor after him was there such a person who could so successfully combine philosophy and politics.",
+                    podcastAuthor: "Elen Jhosh",
+                    podcastImage: "/images/Meghan-and-Harry.jpeg",
+                    podcastDate: "Feb 12"
                 },
                 {
-                    articleTitle: "5 Things White People Keep Getting Totally Wrong About Racism",
-                    articleExcerpt: "They want to escape the harsh, unforgiving glare of culpability by pretending they've done nothing wrong, that if there are ongoing issues with racism in this country, it has nothing to do with how they raise their children. It's because black people refuse to move on.",
-                    articleAuthor: "Elen Jhosh",
-                    articleImage: "/images/Meghan-and-Harry.jpeg",
-                    articleDate: "Feb 12"
-                },
-                {
-                    articleTitle: "In London, the Most Famous \"Great Women Artist\" Finally Gets Her Due",
-                    articleExcerpt: "In July 2018, London's National Gallery Acquired a self-portrait by the Baroque artist Artemisia Gentileschi, featuring the painter in the guise of Sait Catherine of Alexandria. Almost immediately, it drew an unpresedented level of media attention.",
-                    articleAuthor: "Elen Jhosh",
-                    articleImage: "/images/Meghan-and-Harry.jpeg",
-                    articleDate: "Feb 12"
+                    podcastTitle: "What is the tragedy behind Vrubel's paintings?",
+                    podcastExcerpt: "The work of mikhail  Vrubel(1856-1910) became the quintessence of the era in which he lived - a turning point, full of contradictions and collisions.",
+                    podcastAuthor: "Elen Jhosh",
+                    podcastImage: "/images/Meghan-and-Harry.jpeg",
+                    podcastDate: "Feb 12"
                 }
             ]
         }
@@ -37,27 +30,27 @@ export default {
 }
 </script>
 <template>
-    <div class="latest__articles">
+    <div class="latest__podcasts">
         <div class="latest__header">
-            <h2 class="latest__header__title">The Latest Articles</h2>
+            <h2 class="latest__header__title">The Latest podcasts</h2>
             <a href="#">View all</a>
         </div>
-        <article v-for="latestArticle in latestArticles" class="latest__article">
+        <article v-for="latestPodcast in latestPodcasts" class="latest__podcast">
             <figure>
-                <img class="article__img" :src="latestArticle.articleImage" alt="">
+                <img class="podcast__img" :src="latestPodcast.podcastImage" alt="">
             </figure>
-            <div class="latest__article__content">
-                <div class="article__author">
-                    <p class="article__author__name">By {{ latestArticle.articleAuthor }}</p>
+            <div class="latest__podcast__content">
+                <div class="podcast__author">
+                    <p class="podcast__author__name">By {{ latestPodcast.podcastAuthor }}</p>
                 </div>
-                <h3 class="article__title">{{ latestArticle.articleTitle }}</h3>
-                <p class="article__excerpt">{{ latestArticle.articleExcerpt }}</p>
+                <h3 class="podcast__title">{{ latestPodcast.podcastTitle }}</h3>
+                <p class="podcast__excerpt">{{ latestPodcast.podcastExcerpt }}</p>
             </div>
         </article>
     </div>
 </template>
 <style scoped>
-.latest__articles {
+.latest__podcasts {
     margin-bottom: 2rem;
     padding-bottom: 2rem;
 
@@ -83,19 +76,19 @@ export default {
     font-size: 1.1rem;
 }
 
-.latest__article {
+.latest__podcast {
     display: grid;
     grid-template-columns: 1fr 2fr;
     gap: 2rem;
     margin-bottom: 2rem;
 }
 
-.article__author {
+.podcast__author {
     margin-bottom: 1.5rem;
     position: relative;
 }
 
-.article__author::before {
+.podcast__author::before {
     content: "";
     display: block;
     position: absolute;
@@ -105,56 +98,62 @@ export default {
     top: 50%;
 }
 
-.article__author__name {
+.podcast__author__name {
     padding-left: 40px;
     color: grey;
     text-transform: uppercase;
 }
 
-.article__title {
+.podcast__title {
     font-size: 1.8rem;
 }
 
-.article__img {
+.podcast__img {
     width: 350px;
     aspect-ratio: 4/3;
     object-fit: cover;
 }
 
-.article__excerpt {
+.podcast__excerpt {
     color: grey;
     margin-top: 1rem;
 }
 
 @media only screen and (max-width:1024px) {
-    .article__author {
+    .podcast__author {
         margin-bottom: 1rem;
     }
 
-    .article__title {
+    .podcast__title {
         font-size: 1.5rem;
     }
 }
 
 @media only screen and (max-width:800px) {
-    .article__img {
+    .podcast__img {
         width: 320px;
     }
 
-    .article__title {
+    .podcast__title {
         font-size: 1.3rem;
     }
 
 }
 
 @media only screen and (max-width:766px) {
-    .latest__article {
+    .latest__podcast {
         grid-template-columns: 1fr;
     }
+    .latest__header{
+        justify-content: center;
+    }
 
-    .article__img {
+    .podcast__img {
         width: 100%;
         height: 280px;
+    }
+    .latest__header a{
+        display:none;
     }
 
     .latest__header {
